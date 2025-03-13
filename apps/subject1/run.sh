a6a28468ba5290739f58107e51aa4c5e392adf7c
taiduc1001 <phantaiduc2005@gmail.com>
2025-03-13 11:26:49 +0700

    Chores: Reconstruct

--- apps/subject1/run.sh
+++ apps/subject1/run.sh
@@ -1,13 +1,13 @@
-cd custom
+cd settings/parser
 make
 cd -
 GST_DEBUG=3 python3 main.py \
 -i \
-file:///home/nvidia/deepstream_python_apps/apps/subject1/videos/DongKhoi_MacThiBuoi.mp4 \
-file:///home/nvidia/deepstream_python_apps/apps/subject1/videos/RachBungBinh_NguyenThong_2.mp4 \
-file:///home/nvidia/deepstream_python_apps/apps/subject1/videos/TranHungDao_NguyenVanCu.mp4 \
-file:///home/nvidia/deepstream_python_apps/apps/subject1/videos/TranKhacChan_TranQuangKhai.mp4
-# file:///home/nvidia/deepstream_python_apps/apps/subject1/videos/tracking/DongKhoi_MacThiBuoi.mp4 \
-# file:///home/nvidia/deepstream_python_apps/apps/subject1/videos/tracking/RachBungBinh_NguyenThong_2.mp4 \
-# file:///home/nvidia/deepstream_python_apps/apps/subject1/videos/tracking/TranHungDao_NguyenVanCu.mp4 \
-# file:///home/nvidia/deepstream_python_apps/apps/subject1/videos/tracking/TranKhacChan_TranQuangKhai.mp4 \
+file:///home/nvidia/deepstream_python_apps_subject2/apps/subject1/input/DongKhoi_MacThiBuoi.mp4 \
+file:///home/nvidia/deepstream_python_apps_subject2/apps/subject1/input/RachBungBinh_NguyenThong_2.mp4 \
+file:///home/nvidia/deepstream_python_apps_subject2/apps/subject1/input/TranHungDao_NguyenVanCu.mp4 \
+file:///home/nvidia/deepstream_python_apps_subject2/apps/subject1/input/TranKhacChan_TranQuangKhai.mp4
+# file:///home/nvidia/deepstream_python_apps_subject2/apps/subject1/input/tracking/DongKhoi_MacThiBuoi.mp4 \
+# file:///home/nvidia/deepstream_python_apps_subject2/apps/subject1/input/tracking/RachBungBinh_NguyenThong_2.mp4 \
+# file:///home/nvidia/deepstream_python_apps_subject2/apps/subject1/input/tracking/TranHungDao_NguyenVanCu.mp4 \
+# file:///home/nvidia/deepstream_python_apps_subject2/apps/subject1/input/tracking/TranKhacChan_TranQuangKhai.mp4 \
